b2bfdd7a505a18507295354426973a2182e9901c
Pavel-Obshatko <55159101+Pavel-Obshatko@users.noreply.github.com>
2019-09-20 02:50:41 +0300

    Add files via upload

---
+++ Lesson 3.py
@@ -0,0 +1,50 @@
+# задание 1 easy
+
+# def user(name, age, city):
+#     result = '{}, {} год(а), проживает в городе {}'.format(name, age, city)
+#     return result
+
+
+# name = input('Введите ваше имя: ')
+# age = int(input('Введите ваш возраст: '))
+# city = input('Введите ваш город: ')
+
+# result = user(name, age, city)
+# print(result)
+
+# задание 2 easy
+
+# def max_number(num1, num2, num3):
+#     num_list = [num1, num2, num3]
+#     max_num = (max(num_list))
+#     return max_num
+
+# number_1 = int(input('Введите первое число: '))
+# number_2 = int(input('Введите второе число: '))
+# number_3 = int(input('Введите третье число: '))
+
+# result = max_number(number_1, number_2, number_3)
+# print('Максимальное число: {}'.format(result))
+
+# задание 3 easy
+
+# def max_str(* args):                    # list_a = ['zz', 'xxx', 'aaaaa', '444']
+#     max_len = max (args, key=len)       # print(max(list_a, key=len))
+#     return max_len
+
+# str_1 = input('Введите первую строку: ')
+# str_2 = input('Введите вторую строку: ')
+# str_3 = input('Введите третью строку: ')
+
+# result = max_str(str_1, str_2, str_3)
+# print('Самая длиная строка: {}'.format(result))
+
+# задание 1 normal
+
+
+# names = ['Иван', 'Александр', 'Петр', 'Владимир', 'Олег']
+# salary = [30000, 40000, 25000, 37000, 67000]
+
+
+
+
